04a5b53086951a378a9c397bdab4ac34526d8eb5
NiranjanaAneesh <niranjanaaneeessh@gmail.com>
2025-04-10 15:13:58 +0530

    howdy

---
+++ AfterClass.py
@@ -0,0 +1,8 @@
+print("Enter your name:")
+name = input("Enter your name:")
+message = "\nHello "+ name + "\nCongratulations on your amazing achievement! You deserve all the success."
+print(message.upper())
+
+print("Good Morning Ma'am.\nI needed my father's assistance because I couldn't finish this project on my own. He suggested me to add the + signs before and after the name(on line number 3) because the project was giving me an error.")
+
+
