a17e6e1f78f277bf35cc7f34e33977980c0fa749
NiranjanaAneesh <niranjanaaneeessh@gmail.com>
2025-04-10 17:53:37 +0530

    howdy

--- AfterClass.py
+++ AfterClass.py
@@ -1,6 +1,6 @@
 print("Enter your name:")
 name = input("Enter your name:")
-message = "\nHello ", name , "\nCongratulations on your amazing achievement! You deserve all the success."
+message = "\nHello "+ name + "\nCongratulations on your amazing achievement! You deserve all the success."
 print(message.upper())
 
 
