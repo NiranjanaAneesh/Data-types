3a421b7da16ac1b92cffc8dd10169052d9fc24f4
NiranjanaAneesh <niranjanaaneeessh@gmail.com>
2025-04-10 14:43:38 +0530

    hey

---
+++ StringOperation.py
@@ -0,0 +1,14 @@
+str1 = 'Hello '
+str2 = 'Codingal World!'
+
+#using +
+print('str1 + str2 =', str1 + str2)
+
+
+Str = "string"
+print(Str[::3])
+print(Str[0::1])
+print(Str[::-1])
+
+message = "python is fun!"
+print(message.upper())
